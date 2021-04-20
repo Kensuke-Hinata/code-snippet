d00bb26df72acbad9389b9817a35c34ddf49a312
Kensuke-Hinata <cpy4ever@gmail.com>
2021-04-20 10:34:42 +0800

    add spark/learning-spark-lightning-fast-big-data-analysis/jvm/src/main/java/redocpot/spark/LineFilter.java
    Change-Id: I1952fad46b5e61608845833c823fee16acf741e9

---
+++ spark/learning-spark-lightning-fast-big-data-analysis/jvm/src/main/java/redocpot/spark/LineFilter.java
@@ -0,0 +1,23 @@
+package redocpot.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class LineFilter {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf()
+            .setMaster("local")
+            .setAppName("LineFilter");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> textFile = sc.textFile(
+                "/Users/me/Code/spark-learning/learning-spark-lightning-fast-big-data-analysis/pom.xml");
+        JavaRDD<String> xmlLines = textFile.filter(line -> line.contains("xml"));
+        xmlLines.foreach(line -> {
+            System.out.println(line);
+        });
+
+        sc.stop();
+    }
+}
