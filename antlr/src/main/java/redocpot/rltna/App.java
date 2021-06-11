bb9a32f333cf2d66bfc98bfce2c630c0f5df09bc
Kensuke-Hinata <cpy4ever@gmail.com>
2021-06-11 11:16:17 +0800

    add antlr/src/main/java/redocpot/rltna/App.java
    Change-Id: I359bd2de37db0ad0c19bc4626d55dc935ed6164a

---
+++ antlr/src/main/java/redocpot/rltna/App.java
@@ -0,0 +1,11 @@
+package redocpot.rltna;
+
+/**
+ * Welcome to ANTLR!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println("Welcome to ANTLR!");
+    }
+}
